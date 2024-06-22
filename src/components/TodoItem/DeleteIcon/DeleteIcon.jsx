9a0ad1b9fb55cb1692a687cce0dbb49ed671c6ae
Nazareno <nazarenorosales_@hotmail.com>
2024-06-22 19:53:14 -0300

    just changing icons colors

--- src/components/TodoItem/DeleteIcon/DeleteIcon.jsx
+++ src/components/TodoItem/DeleteIcon/DeleteIcon.jsx
@@ -1,13 +1,13 @@
 import { ImCross } from "react-icons/im";
 import styles from "./DeleteIcon.module.css";
-const DeleteIcon = ({ removeTask, i,  }) => {
+const DeleteIcon = ({ removeTask, i }) => {
   return (
     <span
       onClick={() => {
         removeTask(i);
       }}
     >
-      <ImCross className={styles.icon} />
+      <ImCross className={styles.icon}  />
     </span>
   );
 };
